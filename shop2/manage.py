549620a86724c9cbf706e10bce0a51e01f68ef57
Sun In <inthesun@knou.ac.kr>
2022-09-27 15:51:53 +0900

    add log.txt during initializing, and echo DB info

--- shop2/manage.py
+++ shop2/manage.py
@@ -74,9 +74,17 @@ def db_info(host, user, password, db):
         "password": password,
         "database": db
     }, open("database.json", "w"))
-    
+    str_check_info = "[CHECK DB INFOMATION] \n[host= " + host + "]\n[user= " + user + "] \n[db= " + db + "]"
+    click.echo(str_check_info)
     click.echo("데이터베이스 정보가 잘 생성되었습니다")
 
+@cli.command()
+@click.option('--host', default="", help='DB Host')
+@click.option('--user', default="", help='DB User')
+@click.option('--password', default="", help='DB User Password')
+@click.option('--db', default="", help='DB Name')
+def check_info(host, user, password, db):
+    click.echo("host= ", host, "    user= ", user, " password= ", password, "    db= ", db)
 
 if __name__ == '__main__':
     cli()
